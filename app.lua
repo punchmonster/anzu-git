93fa3c4670541ead099ebc95b858d1f26e05c665
punchmonster <igloman88@gmail.com>
2019-04-12 07:22:29 +0200

    signup page

--- app.lua
+++ app.lua
@@ -10,7 +10,7 @@ app.layout = require "views.layout"
 
 -- controllers
 local index       = require "controllers.index"
-local contact     = require "controllers.contact"
+local login       = require "controllers.login"
 local error       = require "controllers.error"
 local error_404   = require "controllers.error_404"
 local feed        = require "controllers.feed"
@@ -22,7 +22,7 @@ app.handle_404 = error_404
 
 -- controller routes
 app:match("index", "/",                                         index)
-app:match("contact", "/contact",                                contact)
+app:match("login", "/login",                                    login)
 app:match("error", "/error/:errorCode",                         error)
 app:match("404", "/404",                                        error_404)
 app:match("feed", "/feed/:feedName[%a](/page/:pageNumber[%d])", respond_to(feed))
--- controllers/login.lua
+++ controllers/login.lua
@@ -3,10 +3,11 @@ local Feeds       = require "models.feeds"
 return function(self)
 
   -- Page title
-  self.page_title = "contact"
+  self.page_title = "login"
 
   -- get all feed info
+  self.header_vis = false
   self.nav_data = Feeds:get_all()
 
-  return { render = "contact" }
+  return { render = "login" }
 end
