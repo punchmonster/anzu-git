a7ce3cd0517bad74f646794ea0ad9b9e004ffeae
punchmonster <igloman88@gmail.com>
2019-04-16 10:28:57 +0200

    fix

--- models/posts.lua
+++ models/posts.lua
@@ -139,7 +139,7 @@ end
 -- RETURN: table with posts
 function Posts:get_timeline(following)
 
-  local processedFollowing = following
+  local processedFollowing = following[1]
   for k, v in pairs(following) do
     processedFollowing = ProcessedFollowing .. "," .. v
   end
