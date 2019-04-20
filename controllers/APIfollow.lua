59b66aae4f02807e3545c802cf6d5716b074f1ab
punchmonster <igloman88@gmail.com>
2019-04-20 14:45:21 +0200

    woops

--- controllers/APIfollow.lua
+++ controllers/APIfollow.lua
@@ -2,7 +2,7 @@ local User  = require "models.user"
 
 return function(self)
 
-  function User:follow(self.params.userHandle, self.params.followHandle)
+  User:follow(self.params.userHandle, self.params.followHandle)
 
   return { render = "error_404", status = "404" }
 end
