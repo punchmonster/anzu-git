2ddf65c06ace14a264008d95ec6956740241244a
punchmonster <igloman88@gmail.com>
2019-05-04 19:46:38 +0200

    typo

--- controllers/settings.lua
+++ controllers/settings.lua
@@ -12,7 +12,7 @@ return {
 
   GET = function(self)
 
-    if self.loggedIn == "true" then
+    if self.loggedIn == true then
 
       -- generate token for page verification
       self.csrf_token = csrf.generate_token(self)
@@ -42,35 +42,11 @@ return {
       -- create cookie
       if status == true then
         self.session.userHandle = self.params.userHandle
-        self.session.current_user = userID
-        self.session.sessionID = sessionID
       end
 
       return msg
     else
 
-      -- user sign up
-
-      -- spam detection
-      if #self.params.email > 1 then
-        return { redirect_to = self:url_for("error", { errorCode = "err_not_allowed" }) }
-      end
-
-      -- check username and password length + formatting
-      if #self.params.userHandle < 3 or #self.params.userHandle > 15 then
-        return { redirect_to = self:url_for("error", { errorCode = "err_not_allowed" }) }
-      end
-
-      if #self.params.userPassword < 7 or #self.params.userPassword > 64 then
-        return { redirect_to = self:url_for("error", { errorCode = "err_not_allowed" }) }
-      end
-
-      -- check csrf protection
-      print(csrf.assert_token(self))
-
-      -- verification
-      local status, msg = User:create(self.params.userHandle, self.params.userPassword)
-      return msg
     end
   end
 }
