a3f4dd8b9e866d899a57434e1d4fa32af9a97e82
punchmonster <igloman88@gmail.com>
2019-05-09 22:46:30 +0200

    test

--- API/PKI.lua
+++ API/PKI.lua
@@ -3,8 +3,6 @@ local User  = require "models.user"
 return function(self)
 
 
-  return "EC62558EA8FB4F315B9DB1FFA43663C05E0B86063EE64AFFCFFF9B9550A56392
-comodoca.com
-555p3t55TUP5DU557bF5"
+  return "EC62558EA8FB4F315B9DB1FFA43663C05E0B86063EE64AFFCFFF9B9550A56392\ncomodoca.com\n555p3t55TUP5DU557bF5"
   --tostring(status) .. ": " .. msg
 end
