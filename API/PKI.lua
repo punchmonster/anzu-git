d3af0720c4d7662d29148b43d6f149e1bab923ac
punchmonster <igloman88@gmail.com>
2019-05-09 23:02:47 +0200

    remove PKI

--- API/PKI.lua
+++
@@ -1,8 +0,0 @@
-local User  = require "models.user"
-
-return function(self)
-
-
-  return { redirect_to = self:url_for("login") }
-  --tostring(status) .. ": " .. msg
-end
--- app.lua
+++ app.lua
@@ -23,7 +23,6 @@ local settings    = require "controllers.settings"
 local APIfollow   = require "API.follow"
 local APIlikes    = require "API.likes"
 local APItags     = require "API.tags"
-local PKI         = require "API.PKI"
 
 -- before routes
 app:before_filter(check_auth)
@@ -43,6 +42,5 @@ app:match("settings", "/settings",                              respond_to(setti
 app:match("follow", "/API/follow/:followHandle(/:toggle)",      APIfollow)
 app:match("likes", "/API/likes/:option/:ID[%d]",                APIlikes)
 app:match("tags", "/API/tags/:option/:ID[%d]",                  APItags)
-app:match("PKI", " /.well-known/pki-validation/A849D3C04D0CA7AC94C1E758FE15643E.txt", PKI)
 
 return app
