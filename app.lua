d43515c949710c17adc1aa56641646a03311f35b
punchmonster <igloman88@gmail.com>
2019-05-17 03:32:52 +0200

    move themes into APIs

--- app.lua
+++ app.lua
@@ -20,16 +20,17 @@ local notifications = require "controllers.notifications"
 local profile       = require "controllers.profile"
 local error_404     = require "controllers.error_404"
 local settings      = require "controllers.settings"
-local change_theme  = require "controllers.change_theme"
+
 
 -- mobile
 local mobile_login  = require "mobile.mobile_login"
 
 -- API controllers
-local APIfollow   = require "API.follow"
-local APIlikes    = require "API.likes"
-local APIposts    = require "API.posts"
-local APItags     = require "API.tags"
+local APIfollow    = require "API.follow"
+local APIlikes     = require "API.likes"
+local APIposts     = require "API.posts"
+local APItags      = require "API.tags"
+local APItheme     = require "API.theme"
 
 -- before routes
 app:before_filter(check_auth)
@@ -46,13 +47,15 @@ app:match("login", "/login",                                    respond_to(login
 app:match("notifications", "/notifications",                    respond_to(notifications))
 app:match("profile", "/:userHandle(/:postID[%d])",              respond_to(profile))
 app:match("settings", "/settings",                              respond_to(settings))
-app:match("change_theme", "/s/change_theme",                    change_theme)
+app:match("search", "/search(/:searchString)",                  respond_to(search))
+app:match("profile", "/:userHandle(/:postID[%d])",              respond_to(profile))
 
 -- API routes
 app:match("follow", "/API/follow/:followHandle(/:toggle(/:APIKey))",  APIfollow)
 app:match("likes", "/API/likes/:option/:ID[%d](/:APIKey)",            APIlikes)
 app:match("posts", "/API/posts/:option(/:ID(/:ID2))",                 APIposts)
 app:match("tags", "/API/tags/:option/:ID[%d](/:APIKey)",              APItags)
+app:match("theme", "/API/change_theme",                                 APItheme)
 
 -- mobile routes
 app:match("mobile_login", "/m/mobile_login", respond_to(mobile_login))
